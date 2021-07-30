32a09dccbfb2dfaae8d14239b5c6362f33e27671
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-07-30 21:47:04 +0600

    Solved 1004

---
+++ 1004.c
@@ -0,0 +1,10 @@
+// Read two integer values. After this, calculate the product between them and store the result in a variable named PROD.
+
+#include<stdio.h>
+int main(){
+    int A,B,PROD;
+    scanf("%d%d",&A,&B);
+    PROD = A * B;
+    printf("PROD = %d\n", PROD);
+    return 0;
+}
