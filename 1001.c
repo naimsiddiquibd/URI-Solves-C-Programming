41ca363a772af1d85d44ccbbd4abb32b7937bf7f
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-07-30 21:38:32 +0600

    Solved 1003

--- 1001.c
+++ 1001.c
@@ -1,3 +1,6 @@
+// Read 2 variables, named A and B and make the sum of these two variables, assigning
+// its result to the variable X. Print X as shown below.
+
 #include <stdio.h>
 int main(){
     int A,B,X;
